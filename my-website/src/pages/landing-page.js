569c4ef8cfceaa64fecb83e17fc8477f90d1e7da
Aqsaali7 <aaqsa5411@gmail.com>
2025-12-08 00:02:21 +0500

    Fix landing-page components for SSG

---
+++ my-website/src/pages/landing-page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function LandingPage() {
+  return (
+    <Layout
+      title="Landing Page"
+      description="Description will go into a meta tag in <head />">
+      <main>
+        <h1>Welcome to the Landing Page!</h1>
+        <p>This is a placeholder page.</p>
+      </main>
+    </Layout>
+  );
+}
+
+export default LandingPage;
